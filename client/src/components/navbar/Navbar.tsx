24ba922a6150fe153f531336a8d68d0dfa4f68e9
Intelliapps <41976910+intelliapps-io@users.noreply.github.com>
2019-10-16 19:23:59 -0400

    created new event form

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -25,9 +25,8 @@ const _Navbar: React.FC<NavbarProps> = props => {
 
   function UserMenu() {
     return [
-      <Menu.Item key="1">nav 1</Menu.Item>,
-      <Menu.Item key="2">nav 2</Menu.Item>,
-      <Menu.Item key="3">nav 3</Menu.Item>,
+      <Menu.Item key="/events"><Link to="/events"><Icon type="calendar"/> Events</Link></Menu.Item>,
+      <Menu.Item key="/account"><Link to="/account"><Icon type="user"/> Account</Link></Menu.Item>,
       <Menu.Item key="4" className="account" onClick={() => handleLogout()}>
         <Icon type="logout" /> Logout
       </Menu.Item>
