dd632833dcf4d472f6971c7fabde7bf4af32e8a1
Dom Mentee <chuku0929@DomsMacBP.fios-router.home>
2020-01-07 22:02:59 -0500

    account

--- client/src/pages/account/Account.tsx
+++ client/src/pages/account/Account.tsx
@@ -1,45 +1,22 @@
-import React from "react";
-import { useUsersQuery } from "../../lib/codegen";
+import React, { useContext } from "react";
+import { } from "../../lib/codegen";
 import { Spin, Alert, List, Typography } from "antd";
+import { AppContext } from "../../lib/helpers/AppContext";
 
 interface IProps {
 
 }
 
 export const Account: React.FC<IProps> = props => {
-  const { loading, error, data } = useUsersQuery({
-    variables: {
-      data: {
-        limit: 10,
-        offset: 0
-      }
-    }
-  })
-
-  if (loading)
+  const {user} = useContext(AppContext);
+  if (!user)
     return <Spin />
-  if (error)
-    return <Alert message={error.message} type="error" />
 
-  const users = data ? data.users.items : []
-  
   return (
     <div>
       <h2>Account</h2>
-      <List
-        bordered
-        dataSource={users}
-        header={<h3>Users List</h3>}
-        renderItem={user => (
-          <List.Item>
-            <Typography.Text mark>{user.role}</Typography.Text> {user.email}
-          </List.Item>
-        )}
-        style={{
-          width: 500,
-          margin: 10
-        }}
-      />
+      <h3>{user.firstName}</h3>
+      
     </div>
   );
 }
