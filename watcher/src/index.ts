e3127cfce847dd2809c58cddb8822633753433a4
Intelliapps <41976910+intelliapps-io@users.noreply.github.com>
2020-01-19 18:28:50 -0500

    added line

--- watcher/src/index.ts
+++ watcher/src/index.ts
@@ -4,6 +4,9 @@ const main = async () => {
   const port = 9001, guid = '2g948693-12dc-406a-8927-r753z56jd489'
   const app = express()
 
+  app.post('github', (req, res) => {
+    console.log(req)
+  })
   
   app.listen(port)
   console.log(`Watcher listening on port ${9001}`)
