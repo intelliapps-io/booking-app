7f1e3bb6811a39d9fedffcaf6d88fdada99aa3a1
Intelliapps <41976910+intelliapps-io@users.noreply.github.com>
2020-01-20 11:04:48 -0500

    make deploy.sh run from git clone, not local file

--- docker/build-files/deploy.sh
+++ docker/build-files/deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 # install and build server
-cd /var/node-app/server && npm i;
+cd /var/node-app/server;
+npm i;
 npm run gen;
 npm run build;
 
 # install and build client
-cd /var/node-app/client && yarn install;
+cd /var/node-app/client;
+yarn install;
 yarn build;
 
 # install and build watcher
-cd /var/node-app/watcher && yarn install;
+cd /var/node-app/watcher;
+yarn install;
 yarn build;
