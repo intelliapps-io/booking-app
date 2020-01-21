3005619a81426c802dee344ad4aa7fc47d2c6c7b
Intelliapps <41976910+intelliapps-io@users.noreply.github.com>
2020-01-21 01:02:13 -0500

    added server features

--- watcher/src/index.ts
+++ watcher/src/index.ts
@@ -3,46 +3,56 @@ import express from 'express'
 import crypto from 'crypto'
 import pm2 from 'pm2'
 import { rebuildApp } from './rebuildApp';
-import { getPm2Process } from './helpers';
+import { getPm2Process, nodeLogger } from './helpers';
+import CreateSocketIO from 'socket.io'
 
 const main = async () => {
+  const app = express(), port = process.env.PORT, guid = '2g948693-12dc-406a-8927-r753z56jd489'
   if (!process.env.PORT)
-    return console.error('NO PORT PROVIDED AS ENV VAR')
+    return nodeLogger('NO PORT PROVIDED AS ENV VAR')
 
-  const port = process.env.PORT, guid = '2g948693-12dc-406a-8927-r753z56jd489'
-  const app = express()
+  const server = require('http').Server(app)
+  const io = CreateSocketIO(server)  
 
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: false }));
 
+  // GitHub WebHook
   app.post('/github', (req, res) => {
     if (req.body.ref !== 'refs/heads/master')
       return
     const signature = req.headers['x-hub-signature'];
     const hash = `sha1=${crypto.createHmac('sha1', guid).update(JSON.stringify(req.body)).digest('hex')}`
     if (hash !== signature)
-      return console.log('hash !== signature')
+      return nodeLogger('hash !== signature')
+    nodeLogger('Git Push Made and Accepts')
     // hanlde pm2 update
-    rebuildApp()
-      .catch(err => console.log('ERROR BUILDING APP'))
+    rebuildApp(io)
+      .catch(err => nodeLogger('ERROR BUILDING APP'))
       .then(async () => {
-        const pm2Process = await getPm2Process('Worksoft Systems').catch(err => console.log(err))
+        const pm2Process = await getPm2Process('Worksoft Systems').catch(err => nodeLogger(err))
         if (!pm2Process)
-          return console.log('Error, no pm2Process found to restart')
+          return nodeLogger('Error, no pm2Process found to restart')
         pm2.restart(pm2Process.pm_id!, (err, proc) => {
           if (err)
-            return console.log(err)
+            return nodeLogger(err)
           else
-            console.log('Process Restarted!')
+          nodeLogger('Process Restarted!')
         })
       })
   })
 
-  app.get('/', (req, res) => {
-    res.send('Welcome')
-  })
+  // Index Route
+  app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+  });
 
-  app.listen(port)
+  // Socket IO
+  // setInterval(() => {
+  //   io.emit('data', { hello: 'world' })
+  // }, 1000)
+
+  server.listen(port)
   console.log(`Watcher listening on port http://localhost:${port}`)
 }
 
--- watcher/src/rebuildApp.ts
+++ watcher/src/rebuildApp.ts
@@ -1,24 +1,61 @@
 import cp, { spawn, SpawnOptionsWithoutStdio } from 'child_process'
+import { nodeLogger } from './helpers'
+import { Server } from 'socket.io'
+import fs from 'fs'
 
 type RunOptions = { command: string, commandOptions?: string[], options?: SpawnOptionsWithoutStdio | undefined }
-
-const run = ({ command, commandOptions, options }: RunOptions): Promise<string> =>
-  new Promise((resolve, reject) => {
-    const cmd = spawn(command, commandOptions, options)
-    cmd.stderr.on("data", data => process.stderr.write(data))
-    cmd.stdout.on("data", data => process.stdout.write(data))
-    cmd.on('error', (error) => reject(error))
-    cmd.on("close", code => { if (code !== 0) reject(new Error(`child process exited with code ${code}`)); else resolve(`child process exited with code ${code}`) });
-  })
 
 interface Step {
   name: string,
   options: RunOptions
 }
 
-export function rebuildApp(): Promise<any> {
+const run = ({ command, commandOptions, options }: RunOptions): Promise<{ result: string, streamlogs: string }> =>
+  new Promise((resolve, reject) => {
+    let streamlogs = ''
+    const cmd = spawn(command, commandOptions, options)
+    cmd.stderr.on("data", data => process.stderr.write(data))
+    cmd.stdout.on("data", data => {
+      process.stdout.write(data)
+      streamlogs += streamlogs + '\n' + data
+    })
+    cmd.on('error', (error) => reject(error))
+    cmd.on("close", code => {
+      if (code !== 0) reject(new Error(`child process exited with code ${code}`))
+      else resolve({ result: `child process exited with code ${code}`, streamlogs })
+    });
+  })
+
+const addLog = (line: string, callback?: (err: NodeJS.ErrnoException | null, text: string) => void) => {
+  fs.readFile(__dirname + '/buildlogs.txt', { encoding: 'utf8' }, (err, data) => {
+    if (err) return
+    const concatLines = data + '\n' + line
+    fs.writeFile(
+      __dirname + '/buildlogs.txt',
+      concatLines,
+      { encoding: 'utf8' },
+      (err) => { if (callback) callback(err, concatLines) }
+    )
+  })
+}
+
+const onStep = (line: string, io: Server): Promise<string> => {
+  return new Promise((resolve: (result: string) => void, reject: (err: Error) => void) => {
+    nodeLogger(line)
+    addLog(line, (err, text) => {
+      io.emit('buildlog', {
+        log: text
+      })
+      resolve(text)
+    })
+  })
+}
+
+export function rebuildApp(io: Server): Promise<any> {
   return new Promise(async (resolve: (data?: any) => void, reject: (err: Error) => void) => {
-    console.log('Rebuilding App')
+    fs.writeFile(__dirname + '/buildlogs.txt', '', { encoding: 'utf8' }, () => {})
+    
+    await onStep('Rebuilding App', io)
 
     const steps: Step[] = [
       // Create Temp Directories
@@ -211,7 +248,7 @@ export function rebuildApp(): Promise<any> {
           commandOptions: ['install'],
           options: { cwd: '/var/node-app/client' }
         }
-      },
+      }, 
       {
         name: 'Delete Temp Node App Directory',
         options: {
@@ -223,13 +260,13 @@ export function rebuildApp(): Promise<any> {
 
     for (let i = 0; i < steps.length; i++) {
       const stepNumber = i + 1, step = steps[i], { command, commandOptions, options } = step.options
-      console.log(`[${stepNumber}/${steps.length}] ${step.name}`)
+      await onStep(`[${stepNumber}/${steps.length}] ${step.name}`, io)
       await run({ command, commandOptions, options })
-        .then(res => console.log(`DONE[${stepNumber}/${steps.length}]: ${step.name}`))
+        .then(({ streamlogs }) => onStep(streamlogs, io).then(() => onStep(`DONE[${stepNumber}/${steps.length}]: ${step.name}`, io)))
         .catch(err => reject(err))
     }
     
-    console.log('done building app')
+    onStep('done building app', io)
     resolve()
   })
 }
