1d99c1ad4d2946aabf526a86536162b4d5afd10c
Intelliapps <41976910+intelliapps-io@users.noreply.github.com>
2020-01-21 12:50:50 -0500

    removed dist

---
+++ docker/build-files/watcher.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Git Watcher',
+      script: '/var/node-app/watcher/dist/index.js',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '4G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 9001,
+        DEBUG: 'logger'
+      }
+    }
+  ]
+};
