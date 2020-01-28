f8eb1fb7d96bca3ce6926ec7fbd1d6a9861dc56b
Intelliapps <41976910+intelliapps-io@users.noreply.github.com>
2020-01-27 21:49:03 -0500

    added event query filters

--- server/src/modules/event/QueryEvents.ts
+++ server/src/modules/event/QueryEvents.ts
@@ -1,15 +1,28 @@
-import { Resolver, Query, Authorized, Ctx, ObjectType, InputType, Arg } from "type-graphql";
+import { Resolver, Query, Authorized, Ctx, ObjectType, InputType, Arg, Field } from "type-graphql";
 import { UserRole, User } from "../../entity/User";
 import { Event } from "../../entity/Event";
 import { MyContext } from "../../ts/context";
 import { PaginatedResponse, PaginatedResponseInput } from "../../helpers/graphqlObjects/PaginatedResponse";
+import { joinRelation, isFormatYMD, queryPaginatedResponse } from "../../helpers/helpers";
 
 @ObjectType()
 class PaginatedEventsResponse extends PaginatedResponse(Event) { }
 
 @InputType()
 class QueryEventsInput extends PaginatedResponseInput {
+  @Field({ nullable: true, description: 'YYYY-MM-DD format' })
+  dateBefore?: string
 
+  @Field({ nullable: true, description: 'YYYY-MM-DD format' })
+  dateAfter?: string
+
+  @Field({ nullable: true })
+  customerId?: string
+
+  @Field({ nullable: true })
+  employeeId?: string
+
+  // TODO: add location filter 
 }
 
 @Resolver()
@@ -18,30 +31,58 @@ export class QueryEventsResolver {
   @Authorized([UserRole['CUSTOMER']])
   queryEvents(@Arg('data') data: QueryEventsInput, @Ctx() ctx: MyContext) {
     return new Promise(async (resolve: (result: PaginatedEventsResponse) => void, reject) => {
-      let user = await User.findOne({ where: { id: ctx.req.userId } }).catch(err => reject(err))
-      if (!user) {
-        reject(new Error('error user not found'))
-        return
-      }
+      // find user who made the query
+      const user = await User.findOne({ where: { id: ctx.req.userId } }).catch(err => reject(err))
+      if (!user)
+        return reject(new Error('please login'))
 
-      let events: [Event[], number] | undefined | void = undefined
-      if (user.role === UserRole['ADMIN'])
-        events = await Event.findAndCount({ skip: data.offset, take: data.limit }).catch(error => reject(error))
-      else if (user.role === UserRole['EMPLOYEE'])
-        events = await Event.findAndCount({ skip: data.offset, take: data.limit, where: { employee: { id: user.id } } }).catch(error => reject(error))// finds all events that match emplyee id   
-      else
-        events = await Event.findAndCount({ skip: data.offset, take: data.limit, where: { customer: { id: user.id } } }).catch(error => reject(error))
+      // VALIDATION
+      if (data.dateBefore && !isFormatYMD(data.dateBefore))
+        return reject(new Error(`${data.dateBefore} is not fomatted as YYYY-MM-DD`))
+      
+      if (data.dateAfter && !isFormatYMD(data.dateAfter))
+        return reject(new Error(`${data.dateAfter} is not fomatted as YYYY-MM-DD`))
+      
+      // find customer from query filter
+      const customer = data.customerId ? await User.findOne({ where: { id: data.customerId } }).catch(err => reject(err)) : undefined
+      if (data.customerId && !customer)
+        return reject(new Error('no customer was found with that id'))
+      
+      // find employee from query filter
+      const employee = data.employeeId ? await User.findOne({ where: { id: data.employeeId } }).catch(err => reject(err)) : undefined
+      if (data.employeeId && !employee)
+        return reject(new Error('no employee was found with that id'))
+      
+      // QUERY 
+      const { limit, offset } = data
+      const query = Event.createQueryBuilder()
+      
+      // handle pagination
+      query.take(limit ? limit : 20)
+      query.skip(offset ? offset : 0)
 
-      // different if
-      if (!events) {
-        reject(new Error('events not found'))
-        return
-      }
+      // join relations
+      joinRelation(query, "Event", "customer")
+      joinRelation(query, "Event", "employee")
+      joinRelation(query, "Event", "organization")
 
-      resolve({ items: events[0], total: events[1] })
+      // FILTERS
+      if (customer)
+        query.andWhere(`"Event"."customerId" = '${customer.id}'`)
+      
+      if (employee)
+        query.andWhere(`"Event"."employeeId" = '${employee.id}'`)
+      
+      if (data.dateBefore)
+        query.andWhere(`Event.datetime <= '${data.dateBefore}'`)
+      
+      if (data.dateAfter)
+        query.andWhere(`Event.datetime >= '${data.dateAfter}'`)
+      
+      // get data
+      queryPaginatedResponse(query)
+        .then(result => resolve(result))
+        .catch(err => reject(err))
     })
-
   }
-
-
 }
---
+++ server/src/modules/event/QueryEvents_OLD.ts
@@ -0,0 +1,51 @@
+import { Resolver, Query, Authorized, Ctx, ObjectType, InputType, Arg } from "type-graphql";
+import { UserRole, User } from "../../entity/User";
+import { Event } from "../../entity/Event";
+import { MyContext } from "../../ts/context";
+import { PaginatedResponse, PaginatedResponseInput } from "../../helpers/graphqlObjects/PaginatedResponse";
+
+@ObjectType()
+class PaginatedEventsResponse extends PaginatedResponse(Event) { }
+
+@InputType()
+class QueryEventsInput extends PaginatedResponseInput {
+  dateBefore?: Date
+  dateAfter?: Date
+  customerId?: string
+  employeeId?: string
+  // TODO: add location filter 
+}
+
+@Resolver()
+export class QueryEventsResolver {
+  @Query(type => PaginatedEventsResponse)
+  @Authorized([UserRole['CUSTOMER']])
+  queryEvents(@Arg('data') data: QueryEventsInput, @Ctx() ctx: MyContext) {
+    return new Promise(async (resolve: (result: PaginatedEventsResponse) => void, reject) => {
+      let user = await User.findOne({ where: { id: ctx.req.userId } }).catch(err => reject(err))
+      if (!user) {
+        reject(new Error('error user not found'))
+        return
+      }
+
+      let events: [Event[], number] | undefined | void = undefined
+      if (user.role === UserRole['ADMIN'])
+        events = await Event.findAndCount({ skip: data.offset, take: data.limit }).catch(error => reject(error))
+      else if (user.role === UserRole['EMPLOYEE'])
+        events = await Event.findAndCount({ skip: data.offset, take: data.limit, where: { employee: { id: user.id } } }).catch(error => reject(error))// finds all events that match emplyee id   
+      else
+        events = await Event.findAndCount({ skip: data.offset, take: data.limit, where: { customer: { id: user.id } } }).catch(error => reject(error))
+
+      // different if
+      if (!events) {
+        reject(new Error('events not found'))
+        return
+      }
+
+      resolve({ items: events[0], total: events[1] })
+    })
+
+  }
+
+
+}
